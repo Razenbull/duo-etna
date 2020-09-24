d27666726c23f80fcda87f0fa544532b118bd381
razenbull <chris@8trust.com>
2020-09-25 00:24:08 +0200

    missing style in header

--- components/SubNav.vue
+++ components/SubNav.vue
@@ -2,7 +2,7 @@
   <scrollactive class="sub-nav sticky top-0 bg-white z-10" :offset="300" :duration="300" :scrollOffset="80">
     <ul class="flex flex-col md:flex-row md:justify-between container mx-auto">
       <li v-for="item in items" :key="item.title" class="px-4 font-bold capitalize cursor-pointer my-3 md:my-0"  >
-        <a :href="`#${item.link}`" class="scrollactive-item">{{ item.title }}</a>
+        <a :href="`#${item.link}`" class="scrollactive-item">{{ item.title }}<span></span></a>
       </li>
     </ul>
   </scrollactive>
